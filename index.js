713316756447778a79b0a2bdb3364618505cd742
MahamudulHossain <md.mahamudulhossainn@gmail.com>
2026-01-06 15:04:07 +0600

    initial setup

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const path = require('path');
+const flash = require('connect-flash');
+const session = require('express-session');
+const expressLayouts = require('express-ejs-layouts');
+require('dotenv').config();
+
+
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(expressLayouts);
+app.set('layout', 'layout');
+app.use(flash());
+
+
+mongoose.connect(process.env.MONGO_URL);
+
+app.get('/', (req, res) => {
+ res.send('<h1>Hello World</h1>');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+ console.log(`Example app listening on port ${port}`);
+});
+
