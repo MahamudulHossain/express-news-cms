e0c8fd163ae1d3aef0b3d801aea04e655ec5874b
MahamudulHossain <md.mahamudulhossainn@gmail.com>
2026-01-08 18:18:39 +0600

    Authentication and Authorization

---
+++ middlewares/isLoggedIn.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+const isLoggedIn = (req, res, next) => {
+    try {
+        const token = req.cookies.token;
+        if (!token) {
+            return redirect('/admin/');
+        }
+        const tokenData = jwt.verify(token, process.env.JWT_SECRET);
+        req.role = tokenData.role;
+        req.fullname = tokenData.fullname;
+        next();
+    } catch (error) {
+        return res.redirect('/admin/');
+    }
+};
+
+module.exports = isLoggedIn;
--- routes/admin.js
+++ routes/admin.js
@@ -5,20 +5,24 @@ const userController = require('../controllers/userController')
 const newsController = require('../controllers/newsController')
 const categoryController = require('../controllers/categoryController')
 const commentController = require('../controllers/commentController')
+const isLoggedIn = require('../middlewares/isLoggedIn')
+const isAdmin = require('../middlewares/isAdmin')
 
 // Login
 router.get('/', userController.login);
 router.post('/login', userController.adminLogin);
+
+router.use(isLoggedIn);
 router.get('/dashboard', userController.dashboard);
 router.get('/logout', userController.logout);
 
 // User CRUD
-router.get('/user', userController.userIndex);
-router.get('/user/create', userController.userCreate);
-router.post('/user/store', userController.userStore);
-router.get('/user/edit/:id', userController.userEdit);
-router.post('/user/update/:id', userController.userUpdate);
-router.delete('/user/delete/:id', userController.userDelete);
+router.get('/user', isAdmin,userController.userIndex);
+router.get('/user/create', isAdmin,userController.userCreate);
+router.post('/user/store', isAdmin,userController.userStore);
+router.get('/user/edit/:id', isAdmin,userController.userEdit);
+router.post('/user/update/:id', isAdmin,userController.userUpdate);
+router.delete('/user/delete/:id', isAdmin,userController.userDelete);
 
 // News CRUD
 router.get('/news', newsController.newsIndex);
@@ -29,18 +33,18 @@ router.post('/news/update', newsController.newsUpdate);
 router.get('/news/delete/:id', newsController.newsDelete);
 
 // Category CRUD
-router.get('/category', categoryController.categoryIndex);
-router.get('/category/create', categoryController.categoryCreate);
-router.post('/category/store', categoryController.categoryStore);
-router.get('/category/edit/:id', categoryController.categoryEdit);
-router.post('/category/update', categoryController.categoryUpdate);
-router.get('/category/delete/:id', categoryController.categoryDelete);
+router.get('/category', isAdmin,categoryController.categoryIndex);
+router.get('/category/create', isAdmin,categoryController.categoryCreate);
+router.post('/category/store', isAdmin,categoryController.categoryStore);
+router.get('/category/edit/:id', isAdmin,categoryController.categoryEdit);
+router.post('/category/update', isAdmin,categoryController.categoryUpdate);
+router.get('/category/delete/:id', isAdmin,categoryController.categoryDelete);
 
 // Comment
 router.get('/comments', commentController.commentIndex);
 router.post('/comments/update', commentController.commentUpdate);
 
 // Settings
-router.get('/settings/update', userController.settingsUpdate);
+router.get('/settings/update', isAdmin,userController.settingsUpdate);
 
 module.exports = router
