8cd29bd45b4c125d1faa2d218adf8392c74dd409
MahamudulHossain <md.mahamudulhossainn@gmail.com>
2026-01-17 16:05:49 +0600

    News by category and author

--- controllers/siteController.js
+++ controllers/siteController.js
@@ -1,5 +1,6 @@
 const newsModel = require('../models/News');
 const categoryModel = require('../models/Category');
+const userModel = require('../models/User');
 
 const index = async(req, res) => {
     const news = await newsModel.find()
@@ -11,10 +12,30 @@ const index = async(req, res) => {
 }
 
 const newsByCategory = async(req, res) => {
-   
+    const slug = await categoryModel.findOne({slug: req.params.slug});
+
+    if(!slug){
+        return res.status(400).json({message: 'Category not found'});
+    };
+   const news = await newsModel.find({category: slug._id})
+                                .populate('category',{'name':1,'slug':1})
+                                .populate('author','fullname');
+    const categoryInUse = await newsModel.distinct('category');
+    const newsCategory = await categoryModel.find({_id: {$in: categoryInUse}});                               
+    return res.render('category', {news, newsCategory,slug});
 }
 
 const newsByAuthor = async(req, res) => {
+    const author = await userModel.findOne({_id:req.params.id});
+    if(!author){
+        return res.status(400).json({message: 'Author not found'});
+    };
+   const news = await newsModel.find({author: author._id})
+                                .populate('category',{'name':1,'slug':1})
+                                .populate('author','fullname');
+    const categoryInUse = await newsModel.distinct('category');
+    const newsCategory = await categoryModel.find({_id: {$in: categoryInUse}});                               
+    return res.render('author', {news, newsCategory,author});
     
 }
 
--- routes/frontend.js
+++ routes/frontend.js
@@ -5,7 +5,7 @@ const siteController = require('../controllers/siteController')
 
 router.get('/', siteController.index);
 router.get('/author/:id', siteController.newsByAuthor);
-router.get('/category/:name', siteController.newsByCategory);
+router.get('/category/:slug', siteController.newsByCategory);
 router.get('/single/:id', siteController.singleNews);
 router.get('/search/:id', siteController.searchNews);
 router.post('/single/:id', siteController.addComment);
