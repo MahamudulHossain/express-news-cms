49b89f5249f87d99dbb3dbe3cffc22161644d03f
MahamudulHossain <md.mahamudulhossainn@gmail.com>
2026-01-18 15:49:39 +0600

    settings data used for logo,title and footer

--- middlewares/frontendCommon.js
+++ middlewares/frontendCommon.js
@@ -1,12 +1,15 @@
 const newsModel = require('../models/News');
 const categoryModel = require('../models/Category');
+const settingModel = require('../models/Setting');
 
 module.exports = async (req, res, next) => {
     const categoryInUse = await newsModel.distinct('category');
     const newsCategory = await categoryModel.find({_id: {$in: categoryInUse}});
     const recentNews = await newsModel.find()
                                     .populate('category',{'name':1,'slug':1})
-                                    .sort({createdAt: -1}).limit(5);  
+                                    .sort({createdAt: -1}).limit(5);
+    const settings = await settingModel.findOne();
+    res.locals.settings = settings;                                     
     res.locals.newsCategory = newsCategory;
     res.locals.recentNews = recentNews;
     next();
